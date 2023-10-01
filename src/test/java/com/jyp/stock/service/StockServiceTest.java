0ac90f748fca5c8e1dc8ab58121ce9799b0981b4
JJongmen <pjy90123@gmail.com>
2023-10-01 20:08:10 +0900

    동시에 100개의 요청 발생시 예상된 값 안나옴

--- src/test/java/com/jyp/stock/service/StockServiceTest.java
+++ src/test/java/com/jyp/stock/service/StockServiceTest.java
@@ -4,9 +4,14 @@ import com.jyp.stock.domain.Stock;
 import com.jyp.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,5 +42,28 @@ class StockServiceTest {
         final Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+
+    @Test
+    void 동시에_100개의_요청() throws InterruptedException {
+        final int threadCount = 100;
+        final ExecutorService executorService = Executors.newFixedThreadPool(32);   // 32개의 스레드를 가진 스레드풀 생성
+        final CountDownLatch latch = new CountDownLatch(threadCount);// 100개의 스레드가 모두 종료될 때까지 대기하는 CountDownLatch 생성 (초기값: 100)
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();  // 스레드가 종료될 때마다 CountDownLatch의 값을 1씩 감소시킴
+                }
+            });
+        }
+
+        latch.await();
+
+        final Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - 100 = 0 예상 -> Race condition 발생
+        assertThat(stock.getQuantity()).isEqualTo(0);
     }
 }
